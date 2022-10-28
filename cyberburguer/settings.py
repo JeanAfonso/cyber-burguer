b7f7a0b3a57c984bd6ee30f7334f710d3b406ec5
Jean Afonso <42818833+JeanAfonso@users.noreply.github.com>
2022-10-28 02:50:21 -0300

    Comentando secret key

--- cyberburguer/settings.py
+++ cyberburguer/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-9kxenon=z8nmonqrbo4&g59g6+epz%bqguo)yfkaspxe!zvt^p"
+# SECRET_KEY = "django-insecure-9kxenon=z8nmonqrbo4&g59g6+epz%bqguo)yfkaspxe!zvt^p"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
